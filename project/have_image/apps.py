0cadee6866964c7499e011cda788bfe72d7e9c8a
Zahoor Novman <zahoor.novman@gmail.com>
2023-01-31 21:21:40 +0100

    Models added but migrations not working

---
+++ project/have_image/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HaveImageConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'have_image'
---
+++ project/registration/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from registration.models import Registration
+
+# Register your models here.
+
+admin.site.register(Registration)
+
