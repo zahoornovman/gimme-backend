66b449083e01a2d5576948e0b410fdfda5ffbe49
Zahoor Novman <zahoor.novman@gmail.com>
2023-02-03 16:58:52 +0100

    Feat: Added difference in subject based on messages

--- project/message/views.py
+++ project/message/views.py
@@ -1,5 +1,7 @@
 from django.core.mail import send_mail
-from rest_framework.generics import ListCreateAPIView
+from rest_framework import status
+from rest_framework.generics import ListCreateAPIView, GenericAPIView
+from rest_framework.response import Response
 
 from have.models import Have
 from message.models import Message
@@ -11,45 +13,65 @@ from want.models import Want
 # Create your views here.
 
 
-class ListCreateMessageView(ListCreateAPIView):
+class ListCreateMessageView(GenericAPIView):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
 
-    def perform_create(self, serializer):
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
         sender_have = None
         sender_want = None
         receiver_have = None
         receiver_want = None
         user_profile_of_user = UserProfile.objects.get(user=self.request.user)
         receiver = UserProfile.objects.get(id=self.request.data['receiver_id'])
-        if self.request.data['sender']:
-            if self.request.data['sender']['type'] == 'have':
-                sender_have = Have.objects.get(id=self.request.data['sender']['id'])
+        try:
+            if self.request.data['sender']:
+                if self.request.data['sender']['type'] == 'have':
+                    sender_have = Have.objects.get(id=self.request.data['sender']['id'])
+                else:
+                    sender_want = Want.objects.get(id=self.request.data['sender']['id'])
+            if self.request.data['receiver']:
+                if self.request.data['receiver']['type'] == 'have':
+                    receiver_have = Have.objects.get(id=self.request.data['receiver']['id'])
+                else:
+                    receiver_want = Want.objects.get(id=self.request.data['receiver']['id'])
+
+            sender_email = self.request.user
+            receiver_user_object = User.objects.get(id=receiver.user_id)
+            receiver_email = receiver_user_object.email
+            message_content = self.request.data['content']
+            email_body = f'{message_content} \n Please email to this address if you are interested: {sender_email}'
+
+            if receiver_have:
+                send_mail(
+                    f'New Barter Request for your Have: {receiver_have.title}',
+                    f'{email_body}',
+                    'gimme.switzerland@gmail.com',
+                    [receiver_email],
+                    fail_silently=False,
+                )
+            elif receiver_want:
+                send_mail(
+                    f'New Barter Request for your Want: {receiver_want.title}',
+                    f'{email_body}',
+                    'gimme.switzerland@gmail.com',
+                    [receiver_email],
+                    fail_silently=False,
+                )
             else:
-                sender_want = Want.objects.get(id=self.request.data['sender']['id'])
-        if self.request.data['receiver']:
-            if self.request.data['receiver']['type'] == 'have':
-                receiver_have = Have.objects.get(id=self.request.data['receiver']['id'])
-            else:
-                receiver_want = Want.objects.get(id=self.request.data['receiver']['id'])
+                send_mail(
+                    f'New Barter Request',
+                    f'{email_body}',
+                    'gimme.switzerland@gmail.com',
+                    [receiver_email],
+                    fail_silently=False,
+                )
 
-        sender_email = self.request.user
-        print(receiver)
-        receiver_user_object = User.objects.get(id=receiver.user_id)
-        print(type(receiver_user_object))
-        print(receiver_user_object)
-        email = receiver_user_object.email
-        print(email)
-        content = self.request.data['content']
+            serializer.save(sender_have=sender_have, sender_want=sender_want, receiver_have=receiver_have,
+                            receiver_want=receiver_want, receiver=receiver, sender=user_profile_of_user)
+            return Response(status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
-        send_mail(
-            f'New Barter Request',
-            f'{content}' 
-            f'Please email to this address if interested: {sender_email}',
-            'gimme.switzerland@gmail.com',
-            [email],
-            fail_silently=False,
-        )
-
-        serializer.save(sender_have=sender_have, sender_want=sender_want, receiver_have=receiver_have,
-                        receiver_want=receiver_want, receiver=receiver, sender=user_profile_of_user)
