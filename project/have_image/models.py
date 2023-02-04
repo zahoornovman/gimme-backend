0719aa9bfcbf7cebd4ab1e71f258f5990035bf96
Zahoor Novman <zahoor.novman@gmail.com>
2023-02-04 21:50:03 +0100

    Feat: Images field added to Have and want model

--- project/have_image/models.py
+++ project/have_image/models.py
@@ -3,7 +3,16 @@ from django.db import models
 from have.models import Have
 
 
+def post_directory_path(instance, filename):
+    return f'{instance.want.author.id}/{filename}'
+
+
 # Create your models here.
 
 class HaveImage(models.Model):
     have = models.ForeignKey(to=Have, on_delete=models.CASCADE, related_name='images')
+    images = models.ImageField(upload_to=post_directory_path, blank=True, null=True)
+
+    def __str__(self):
+        return f'ID: {self.id}: Have Image for UserProfile {self.images}'
+
