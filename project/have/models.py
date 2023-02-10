1b659ae6f5ca5da382e5bb9bb87dda107fbe1f13
Zahoor Novman <zahoor.novman@gmail.com>
2023-02-10 16:43:26 +0100

    status field added to want & have model

--- project/have/models.py
+++ project/have/models.py
@@ -13,11 +13,18 @@ class Have(models.Model):
         USED_GOOD = 3
         USED_FAIR = 4
 
+    class Status(models.IntegerChoices):
+        ACTIVE = 1
+        PENDING = 2
+        NOT_ACTIVE = 3
+        GONE = 4
+
     author = models.ForeignKey(to=UserProfile, on_delete=models.CASCADE, related_name='haves')
     tags = models.ManyToManyField(to=Tag, related_name='haves')
     description = models.CharField(max_length=500, blank=True)
     title = models.CharField(max_length=100, blank=False)
     condition = models.IntegerField(choices=Conditions.choices, blank=False)
+    status = models.IntegerField(choices=Status.choices, blank=False)
     wants_for_this_item = models.CharField(max_length=200, blank=True)
     created_time = models.DateTimeField(auto_now_add=True)
     updated_time = models.DateTimeField(auto_now=True)
--- project/want/models.py
+++ project/want/models.py
@@ -13,11 +13,18 @@ class Want(models.Model):
         USED_GOOD = 3
         USED_FAIR = 4
 
+    class Status(models.IntegerChoices):
+        ACTIVE = 1
+        PENDING = 2
+        NOT_ACTIVE = 3
+        FOUND = 4
+
     author = models.ForeignKey(to=UserProfile, on_delete=models.CASCADE, related_name='wants')
     tags = models.ManyToManyField(to=Tag, related_name='wants')
     description = models.CharField(max_length=500, blank=True)
     title = models.CharField(max_length=100, blank=False)
     condition = models.IntegerField(choices=Conditions.choices, blank=True)
+    status = models.IntegerField(choices=Status.choices, blank=False)
     has_for_this_item = models.CharField(max_length=200, blank=True)
     created_time = models.DateTimeField(auto_now_add=True)
     updated_time = models.DateTimeField(auto_now=True)
