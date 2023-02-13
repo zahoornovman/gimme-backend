f31ef0ed3f69e02d6cc37bdcd0441f2c1c8affe4
Zahoor Novman <zahoor.novman@gmail.com>
2023-02-13 17:29:11 +0100

    Page size modified

--- project/gimme/settings.py
+++ project/gimme/settings.py
@@ -184,7 +184,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 4
+    'PAGE_SIZE': 9
 }
 
 SIMPLE_JWT = {
