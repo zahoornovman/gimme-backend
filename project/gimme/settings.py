c32eee5e68ad7fd463078a7f9c994168d708da9e
Zahoor Novman <zahoor.novman@gmail.com>
2023-02-18 20:20:18 +0100

    image storage folder modified

--- project/gimme/settings.py
+++ project/gimme/settings.py
@@ -158,7 +158,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS =[
-    os.path.join(BASE_DIR, 'message/static')
+    os.path.join(BASE_DIR, 'message/sources')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
